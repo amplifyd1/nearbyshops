04ccb8567d784411f19b292655afe0ebc9ef9a3b
wesley sprague <wesleysprague@yahoo.com>
2019-01-21 17:14:36 -0500

    created superuser

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/wesleysprague/code/amplifyd1/pythonprojects/nearbyshops/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
